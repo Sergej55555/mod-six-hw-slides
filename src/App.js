1e1b7ef69462c5d2eb4b7d985541d96eba0233f6
Sergej55555 <smisenkov137@gmail.com>
2024-02-20 23:17:24 +1000

    my mod six slides

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { data } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [car, setCar] = useState(0);
+  const {id, name, description, image} = data[car]; 
+  
+  const nextCar = () => {
+
+  setCar((car => {
+    car ++;
+    if(car > data.length-1) {
+      car = 0;
+    }
+    return car;
+  }))
+  }
+
+  const previousCar = () => {
+    setCar((car => {
+      car --;
+      if (car < 0) {
+      return data.length-1;
+      }
+      return car;
+    }))
+    }
+ 
+  return (<div>
+
+          <div className='container'>
+          <img src={image} width='350px' alt='NYC Hotels'/>
+          </div>
+
+          <div className='container'>
+          <h1>{id} - {name}</h1> 
+          </div>
+
+          <div className='container'>
+          <p>{description}</p>
+          </div>
+
+          <div className='btn container'>
+          <button onClick={previousCar}>Previous car</button>
+          <button onClick={nextCar}>Next car</button>
+          </div>
+
+        </div>
+      )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  
+  {
+    id: 1,
+    name: "Ferrari",
+    description: "Fast and furious!",
+    image: "https://previews.123rf.com/images/xixinxing/xixinxing1308/xixinxing130801888/35988290-%D0%BC%D0%BE%D0%BB%D0%BE%D0%B4%D0%B0%D1%8F-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D0%B0-%D0%B8-%D0%B5%D0%B5-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C.jpg"
+  },
+
+  {
+    id: 2,
+    name: "BMW",
+    description: "The best of the best!",
+    image: "https://previews.123rf.com/images/xixinxing/xixinxing1311/xixinxing131100212/35991647-%D1%81%D0%B5%D1%80%D0%B5%D0%BD%D0%B0-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D0%B0-%D0%B3%D0%BB%D1%8F%D0%B4%D1%8F-%D1%87%D0%B5%D1%80%D0%B5%D0%B7-%D0%BE%D0%BA%D0%BD%D0%BE-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8F-%D0%B2-%D0%BD%D0%BE%D1%87%D0%BD%D0%BE%D0%B9.jpg"
+  },
+
+  {
+    id: 3,
+    name: "Mercedes",
+    description: "Tough and beutiful!",
+    image: "https://previews.123rf.com/images/xixinxing/xixinxing1604/xixinxing160400890/93294767-mature-couple-in-car.jpg"
+  },
+
+  {
+    id: 4,
+    name: "Lamborghini",
+    description: "Life is beutiful with that car",
+    image: "https://media.istockphoto.com/id/1167555913/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D0%BE%D0%B2%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%BE%D0%B5-%D1%81%D0%B8%D0%BD%D0%B5%D0%B5-%D0%BA%D1%83%D0%BF%D0%B5-%D1%81%D0%BF%D0%BE%D1%80%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D0%B9-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C-%D0%B2-%D1%81%D0%B0%D0%BB%D0%BE%D0%BD%D0%B5.jpg?s=2048x2048&w=is&k=20&c=ojFKQULgcnLHui7HGnCQbpyQnCmAOqEGgAQGFZjXqYY="
+  },
+
+  {
+    id: 5,
+    name: "Toyota",
+    description: "Soul music",
+    image: "https://previews.123rf.com/images/xixinxing/xixinxing1308/xixinxing130802988/35991189-businesswoman-driving-and-on-the-phone-in-beijing.jpg"
+  }
+
+]
